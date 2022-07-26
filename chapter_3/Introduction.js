cad07fa6fa169d45fc5aefb0e0af199916cc92f7
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-07-26 10:25:28 +0200

    Chapter 3
    Arrays Introduction

---
+++ chapter_3/Introduction.js
@@ -0,0 +1,17 @@
+//ARRAYS 
+//Arrays é uma ordem/ordenamento. Storage values. 
+
+// @ts-check
+
+const averageTempJan = 31.9;
+const averageTempFev = 35.3;
+const averageTempMar = 42.4;
+const averageTempApr = 52;
+const averageTempMay = 60.8;
+
+console.log('averageTempJan', averageTempJan);
+console.log('averageTempFev', averageTempFev);
+console.log('averageTempMar', averageTempMar);
+console.log('averageTempApr', averageTempApr);
+console.log('averageTempMay', averageTempMay);
+
