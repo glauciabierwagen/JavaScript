2919bf479697f33e439f2cbfd760995bfadcf378
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-08-19 09:49:38 +0200

    Stack
    Methods peek, push, size, isEmpty

--- chapter_4/Stack.js
+++ chapter_4/Stack.js
@@ -3,6 +3,8 @@
 // const Stack = require('../../dist/js/data-structures/stack'); // for node
 // const Stack = stack; // older browsers - remove from html script import: type="module"
 
+// Stack ou pilha = princípio no qual o topo é o final da pilha, enquanto o início é a base. Como um pilha de livros.
+
 const { Stack } = PacktDataStructuresAlgorithms;
 
 const stack = new Stack(); // new StackArray();
@@ -14,10 +16,20 @@ const stack = new Stack(); // new StackArray();
 
 console.log('Stack.isEmpty() =>', stack.isEmpty()); //output true
 
-stack.push(5);
+stack.push(5); // Adicão de novos elementos na pilha
 stack.push(8);
 
-console.log('stack after push 5 and 8 =>', stack.toString()); // Output 5 and 8
+console.log('stack after push 5 and 8 =>', stack.toString()); // Output 5 and 8  - Verificacão dos elementos na pilha
 console.log('stack.peek() =>', stack.peek()); // Output 8 elemento no topo da pilha
+
+stack.push(11); 
+console.log('stack.size() after push 11 =>', stack.size()); // Outputs 3 elements
+console.log('stack after push 5 and 8 =>', stack.toString());
+console.log('stack.isEmpty() =>', stack.isEmpty()); // Ao chamar o método isEmpty deverá true se a pilha estiver vazia e false se tiver elementos
+
+stack.push(15);
+
+
+
 
 //https://github.com/loiane/javascript-datastructures-algorithms/blob/main/examples/chapter04/01-Stack.js
