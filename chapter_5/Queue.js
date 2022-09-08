91bf9c1c8bb3d5bdedff971ccfd1fa70d10f1a08
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-09-08 10:10:42 +0200

    Class Queue

--- chapter_5/Queue.js
+++ chapter_5/Queue.js
@@ -1,3 +1,13 @@
 //QUEUE
 //em FIFO (First In First Out, isto é, o primeiro que entra é o primeiro que sai), também conhecido como princípio do first-come first-served 
 //(o primeiro a chegar é o primeiro a ser servido).
+
+class Queue {
+    constructor() {
+        this.count = 0; // {1}
+        this.lowestCount = 0; // {2}
+        this.items = {}; // {3}
+    }
+}
+
+console.log(Queue);
