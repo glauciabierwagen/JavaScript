58328fc653a1b94fd1937607812d5767ad684cb1
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-09-08 10:04:34 +0200

    Queue

--- chapter_4/Weakmap.js
+++ chapter_4/Weakmap.js
@@ -68,3 +68,50 @@ class Pilha {
     return objString;
   }
 }
+
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = '';
+  while (number > 0) { // {1}
+  rem = Math.floor(number % 2); // {2}
+  remStack.push(rem); // {3}
+  number = Math.floor(number / 2); // {4}
+  }
+  while (!remStack.isEmpty()) { // {5}
+  binaryString += remStack.pop().toString();
+  }
+  return binaryString;
+  }
+
+//console.log(decimalToBinary(233)); // 11101001
+//console.log(decimalToBinary(10)); // 1010
+//console.log(decimalToBinary(1000)); // 1111101000
+
+//Conversor de base
+
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // {6}
+  let number = decNumber;
+  let rem;
+  let baseString = '';
+  if (!(base >= 2 && base <= 36)) {
+  return '';
+  }
+  while (number > 0) {
+  rem = Math.floor(number % base);
+  remStack.push(rem);
+  number = Math.floor(number / base);
+  }
+  while (!remStack.isEmpty()) {
+  baseString += digits[remStack.pop()]; // {7}
+  }
+  return baseString;
+  }
+
+console.log(baseConverter(100345, 2)); // 11000011111111001
+console.log(baseConverter(100345, 8)); // 303771
+console.log(baseConverter(100345, 16)); // 187F9
+console.log(baseConverter(100345, 35)); // 2BW0
---
+++ chapter_5/Queue.js
@@ -0,0 +1,3 @@
+//QUEUE
+//em FIFO (First In First Out, isto é, o primeiro que entra é o primeiro que sai), também conhecido como princípio do first-come first-served 
+//(o primeiro a chegar é o primeiro a ser servido).
